1d33f730ab0b9a3cbf5a5c6a531b70e4b98758dd
marimeli <marimel.casas@gmail.com>
2019-10-12 17:21:57 -0500

    hello world from node

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const http = require('http');
-// createServer is a method that recieves a callback with 2 parameters
-const server = http.createServer(function (req, res) {
-  // req is the request to the server, res is the answer/response to send to the client.
-  res.end('Hello World!!'); // show in the browser
-}).listen(3000); // set.the port
+
+const myRequest = (req, res) => {
+  res.end('Hello World! :)'); // show in the browser
+};
+
+const server = http.createServer(myRequest).listen(3000); 
 
 
