79e0c97228efe31f4bb4f8dea82eb72989fe0e55
marimeli <marimel.casas@gmail.com>
2019-10-12 17:49:31 -0500

    using the object exports

---
+++ commonJs/script1.js
@@ -0,0 +1,7 @@
+//const exports = {};
+
+const arreglo1 = ['Marilyn', 'Melissa'],
+      arreglo2 = [1, 2, 3, 4];
+
+exports.arreglo1 = arreglo1;
+exports.arreglo2 = () => arreglo2.length;
---
+++ commonJs/script2.js
@@ -0,0 +1,4 @@
+const arreglos = require('./script1');
+
+console.log(arreglos.arreglo1);
+console.log(arreglos.arreglo2());
