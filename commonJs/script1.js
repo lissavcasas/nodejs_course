9e99f525a882cb33baade4ceaf3669d2b0a0ff53
marimeli <marimel.casas@gmail.com>
2019-10-12 17:53:04 -0500

    using module exports

--- commonJs/script1.js
+++ commonJs/script1.js
@@ -3,5 +3,8 @@
 const arreglo1 = ['Marilyn', 'Melissa'],
       arreglo2 = [1, 2, 3, 4];
 
-exports.arreglo1 = arreglo1;
-exports.arreglo2 = () => arreglo2.length;
+/* exports.arreglo1 = arreglo1;
+exports.arreglo2 = () => arreglo2.length; */
+
+/* module.exports = arreglo1; */
+module.exports = arreglo2;
--- commonJs/script2.js
+++ commonJs/script2.js
@@ -1,4 +1,7 @@
 const arreglos = require('./script1');
 
-console.log(arreglos.arreglo1);
-console.log(arreglos.arreglo2());
+/* console.log(arreglos.arreglo1);
+console.log(arreglos.arreglo2()); */
+
+console.log(arreglos);
+
