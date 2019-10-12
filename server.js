2d0fbb882fd408237e8290d2b5dc441901a26c17
marimeli <marimel.casas@gmail.com>
2019-10-12 17:28:14 -0500

    write header and responses

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const myRequest = (req, res) => {
-  res.end('Hello World! :)'); // show in the browser
+  res.writeHead(200, {
+    'Content-Type': 'text/html' //write header html
+  });
+  if (req.url === '/') {
+    res.end('Hello World! :)'); 
+  }  
+  if (req.url === '/about') {
+    res.end('This is the About'); 
+  }
 };
 
 const server = http.createServer(myRequest).listen(3000); 
